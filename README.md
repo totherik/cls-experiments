ec1e20e722f1e2ed1339e924bba866ec3f511ac3
ertoth <ertoth@paypal.com>
2013-11-08 14:17:39 -0800

    baboosh

---
+++ README.md
@@ -0,0 +1,7 @@
+##### Continuation Local Storage Testing
+
+Demo of cls with express using middleware that creates a context for use during a
+request as well as exposing it to code executed within the same continuation context.
+
+$ npm install
+$ node .
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+var http = require('http'),
+    assert = require('assert'),
+    express = require('express'),
+    cls = require('./lib/cls'),
+    sample = require('./lib/sample');
+
+var app = express();
+
+// Set up the local storage context
+app.use(cls.middleware());
+
+// Faux `user` middleware
+app.use(function (req, res, next) {
+    var user = {
+        first_name: 'Call',
+        middle_name: 'me',
+        last_name: 'maybe',
+        create_ts: Date.now()
+    };
+
+    req.setLocal('user', user);
+    next();
+});
+
+app.get('/', function (req, res) {
+    var userA, userB;
+
+    userA = req.getLocal('user');
+    userB = sample.doSomethingSync();
+
+    sample.doSomething(function (err, userC) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+
+        assert.strictEqual(userA, userB);
+        assert.strictEqual(userB, userC);
+        res.json(userC);
+    });
+});
+
+http.createServer(app).listen(8000);
